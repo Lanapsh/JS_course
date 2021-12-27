f6a1dcc180a6e28d1cbc7985f36f4cdb3259333b
Lanapsh <lanapsh@gmail.com>
2021-12-27 12:35:19 +0200

    hw4

---
+++ Lesson_4/hw4/js/task1.js
@@ -0,0 +1,41 @@
+// Создать функцию calculate(a, b, sign), где a и b — два числа, sign — знак арифметической операции.
+// 		Функция должна расчитывать результат операции, исходя из переданного ей знака. 
+// 		Функция должна проверять корректность введенных чисел и знака операции.
+// 		Все аргументы для функции принять от пользователя.
+
+function calculate(a, b, sign) {
+    var result;
+    if (isNaN(a) || isNaN (b)){
+        alert("One of arguments is not a number!");
+        return;
+    }
+    switch(sign) {
+        case "+" : {
+            result = a + b;
+            break;
+        }
+        case "-" : {
+            result = a - b;
+            break;
+        }
+        case "/" : {
+            result = a / b;
+            break;
+        }
+        case "*" : {
+            result = a * b;
+            break;
+        }
+        default: {
+            alert("Unsupported operation!");
+            return;
+        }
+    } return result;
+}
+    var a = +prompt ("Please,enter a number.");
+    var b = +prompt ("Please,enter a number.");
+    var sign = prompt ("Please,enter a sign.");
+    result = calculate(a, b, sign);
+        if (result != undefined) {
+            alert(result);
+}
